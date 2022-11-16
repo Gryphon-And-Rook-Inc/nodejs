0a5451ada7dc0b355295511386f63234c3824a40
gryphonandrook <38541934+gryphonandrook@users.noreply.github.com>
2022-11-16 00:35:27 -0700

    Update install.rb

--- recipes/install.rb
+++ recipes/install.rb
@@ -8,7 +8,7 @@ execute 'update apt-get' do
 end
 
 execute 'add nodejs repo' do
-  command 'curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -'
+  command 'curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -'
 end
 
 execute 'install nodejs' do
