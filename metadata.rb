b473742d011950304f9f93f2b587c99e6da77fe1
gryphonandrook <38541934+gryphonandrook@users.noreply.github.com>
2022-11-17 08:58:26 -0700

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'nodejs'
 maintainer        'Gryphon & Rook Inc'
 license           'All rights reserved'
-description       'Installs NodeJs 10'
+description       'Installs NodeJs 12'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '0.1.0'
+version           '0.2.0'
